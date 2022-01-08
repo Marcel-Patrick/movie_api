1b5011fa34d5490fb01c246aa96e3d3e44a762a3
Marcel-Patrick <90202110+Marcel-Patrick@users.noreply.github.com>
2022-01-08 19:21:48 +0100

    Update index.js
    allowed origins to access your API on Heroku

--- index.js
+++ index.js
@@ -42,8 +42,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // using CORS and allowing all domains to make requests
-app.use(cors());
+//app.use(cors());
 
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234','https://fathomless-plains-90381.herokuapp.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 // using auth.js
 let auth = require("./auth.js")(app);
 
